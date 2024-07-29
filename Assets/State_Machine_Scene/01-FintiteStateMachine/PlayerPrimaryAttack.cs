abc637bfe3e1438e10d2b962eb15b1d59afea880
duykhoi1504 <voduykhoi1504@gmail.com>
2024-07-29 23:07:05 +0700

    fix for attack state

--- Assets/State_Machine_Scene/01-FintiteStateMachine/PlayerPrimaryAttack.cs
+++ Assets/State_Machine_Scene/01-FintiteStateMachine/PlayerPrimaryAttack.cs
@@ -4,35 +4,38 @@ using UnityEngine;
 
 public class PlayerPrimaryAttack : PlayerState
 {
-    private int comboCounter;
-    private float lastTimeAttack;
-    private float comboWindow=2;
-    public PlayerPrimaryAttack(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
-    {
-    }
+  private int comboCounter;
+  private float lastTimeAttack;
+  private float comboWindow = 2;
+  public PlayerPrimaryAttack(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+  {
+  }
 
-   public override void Enter()
-    {
-        base.Enter();
-        
-        if(comboCounter>2 || Time.time>lastTimeAttack+comboWindow)
-            comboCounter=0;
-            Debug.Log(comboCounter);
-    }
-    public override void Exit()
-    {
-        base.Exit();
-        comboCounter++;
-        lastTimeAttack=Time.time;
-        Debug.Log(lastTimeAttack);
+  public override void Enter()
+  {
+    base.Enter();
+
+    if (comboCounter > 2 || Time.time > lastTimeAttack + comboWindow)
+      comboCounter = 0;
+
+    player.anim.SetInteger("ComboCounter",comboCounter); 
+    Debug.Log(comboCounter);
+  }
+  public override void Exit()
+  {
+    base.Exit();
+    comboCounter++;
+    lastTimeAttack = Time.time;
+    // Debug.Log(lastTimeAttack);
 
 
-    }
-    public override void Update()
+  }
+  public override void Update()
+  {
+    base.Update();
+    if (triggerCalled)
     {
-        base.Update();
-      if(triggerCalled){
-        stateMachine.ChangeState(player.idleState);
-      }
+      stateMachine.ChangeState(player.idleState);
     }
+  }
 }
